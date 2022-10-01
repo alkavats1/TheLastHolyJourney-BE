e10c183ddeee98cf2dd67d02fd2d95f2f2c1c672
alkavats1 <alka.vats@knoldus.com>
2022-10-01 22:31:26 +0530

    PL-01 | Book Slots

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const formRoute = require('./routes/form')
+const bookSlotroute = require('./routes/bookSlot')
+
+
+const uri = "mongodb+srv://RestApi:Nitin123@cluster0.fmc3i.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+mongoose.connect(uri,(err)=>{
+    if(!err){
+        console.log("success")
+    }else {
+        console.log("Error Connecting")
+    }
+})
+
+const app = express()
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+
+const Port = process.env.Port || 3000;
+
+app.listen(Port, () => {
+    console.log(`server is running on port ${Port}`)
+})
+
+app.use('/api/form', formRoute);
+app.use('/api/bookSlot', bookSlotroute);
+
+
+
+
+
+
+
+
+
+
